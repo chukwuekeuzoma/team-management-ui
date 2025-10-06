d879e0902864a5f0e0f43aed9512f686e0f2bbc1
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-10-06 19:17:54 +0100

    chore: update dependencies and add Tailwind CSS configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1659E6",
+        secondary: "#E43A39",
+        custom: {
+          white: "#FFFFFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
