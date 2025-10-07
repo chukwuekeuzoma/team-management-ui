a13886cac8eca0c8084561ea83015882a7809668
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-10-07 21:14:29 +0100

    chore: remove Navigationbar and StepsNavigationBar from Home component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Navigationbar } from "@/components/dashboard/NavigationBar";
+import { StepsNavigationBar } from "@/components/dashboard/StepsNavigationBar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigationbar />
+        <StepsNavigationBar />
         {children}
       </body>
     </html>
