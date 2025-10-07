1239d122167604bde98267ae4aa924a741edd863
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-10-07 08:15:48 +0100

    chore: add new dependencies and enhance Tailwind CSS configuration with custom colors and animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
@@ -39,7 +40,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            <Button>Deploy now</Button>
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +48,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-           Martins
+            <Button> Martins</Button>
           </a>
         </div>
       </main>
