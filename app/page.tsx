f6bf02dbfc3c104e23e86c5b407cfd092d23fa8c
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-10-07 16:08:07 +0100

    Refactor: code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 // import Image from "next/image";
 import { Button } from "@/components/ui/Button";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/Select";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Button size="lg"> Martins</Button>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="ok" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
